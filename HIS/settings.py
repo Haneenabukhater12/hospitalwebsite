f087e78fdb142cf16aa585f10ecfcba2cd533d25
Aliaa <aliaa.shalaby04@eng-st.cu.edu.eg>
2025-04-25 09:15:45 +0300

    Your commit message

--- HIS/settings.py
+++ HIS/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'his_db',           # pick a name
         'USER': 'postgres',         # your Postgres user
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PAssword'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
